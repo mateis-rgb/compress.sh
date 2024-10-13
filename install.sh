863bb460da3f14ab7c02b2b4fa89279faa30a0cf
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-10-13 16:39:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,30 +11,30 @@ UNINSTALL_URL="https://raw.githubusercontent.com/mateis-rgb/compress.sh/refs/hea
 
 # Vérifier si le script compress.sh existe, sinon le télécharger
 if [ ! -f "compress.sh" ]; then
-    echo -e "\e[33m[WARNING]: \e[37mLe fichier compress.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
+    printf "\e[33m[WARNING]: \e[37mLe fichier compress.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
     curl -o compress.sh "$COMPRESS_URL"
     if [ $? -ne 0 ]; then
-        echo -e "\e[31m[ERROR]: \e[37mÉchec du téléchargement de compress.sh."
+        printf "\e[31m[ERROR]: \e[37mÉchec du téléchargement de compress.sh."
         exit 1
     fi
 fi
 
 # Vérifier si le script install.sh existe, sinon le télécharger
 if [ ! -f "install.sh" ]; then
-    echo -e "\e[33m[WARNING]: \e[37mLe fichier install.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
+    printf "\e[33m[WARNING]: \e[37mLe fichier install.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
     curl -o install.sh "$INSTALL_URL"
     if [ $? -ne 0 ]; then
-        echo -e "\e[31m[ERROR]: \e[37mÉchec du téléchargement de install.sh."
+        printf "\e[31m[ERROR]: \e[37mÉchec du téléchargement de install.sh."
         exit 1
     fi
 fi
 
 # Vérifier si le script uninstall.sh existe, sinon le télécharger
 if [ ! -f "uninstall.sh" ]; then
-    echo -e "\e[33m[WARNING]: \e[37mLe fichier uninstall.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
+    printf "\e[33m[WARNING]: \e[37mLe fichier uninstall.sh n'existe pas dans le répertoire courant. Téléchargement depuis GitHub..."
     curl -o uninstall.sh "$UNINSTALL_URL"
     if [ $? -ne 0 ]; then
-        echo -e "\e[31m[ERROR]: \e[37mÉchec du téléchargement de uninstall.sh."
+        printf "\e[31m[ERROR]: \e[37mÉchec du téléchargement de uninstall.sh."
         exit 1
     fi
 fi
@@ -48,7 +48,7 @@ chmod +x "$INSTALL_DIR/compress.sh" "$INSTALL_DIR/install.sh" "$INSTALL_DIR/unin
 # Supprimer le répertoire courant s'il est vide après le déplacement des fichiers
 CURRENT_DIR=$(pwd)
 if [ -z "$(ls -A "$CURRENT_DIR")" ]; then
-    echo -e "\e[34m[PROGRAM]: \e[37mLe répertoire $CURRENT_DIR est vide. Suppression..."
+    printf "\e[34m[PROGRAM]: \e[37mLe répertoire $CURRENT_DIR est vide. Suppression..."
     rmdir "$CURRENT_DIR"
 fi
 
@@ -60,17 +60,17 @@ elif [ "$SHELL" == "/bin/zsh" ]; then
 elif [ "$SHELL" == "/bin/sh" ]; then
     RC_FILE="$HOME/.shrc"
 else
-    echo -e "\e[31m[ERROR]: \e[37mShell non supporté. Utilisez bash ou zsh."
+    printf "\e[31m[ERROR]: \e[37mShell non supporté. Utilisez bash ou zsh."
     exit 1
 fi
 
 # Vérifier si la ligne est déjà présente
 if grep -q "source $INSTALL_DIR/compress.sh" "$RC_FILE"; then
-    echo -e "\e[34m[PROGRAM]: \e[37mLa ligne est déjà présente dans $RC_FILE."
+    printf "\e[34m[PROGRAM]: \e[37mLa ligne est déjà présente dans $RC_FILE."
 else
     # Ajouter la ligne pour exécuter compress.sh
     echo "alias compress=\"$INSTALL_DIR/compress.sh\"" >> "$RC_FILE"
-    echo -e "\e[34m[PROGRAM]: \e[37mLe script compress.sh a été ajouté à $RC_FILE."
+    printf "\e[34m[PROGRAM]: \e[37mLe script compress.sh a été ajouté à $RC_FILE."
 fi
 
-echo -e "\e[34m[PROGRAM]: \e[37mInstallation terminée. Veuillez redémarrer votre shell ou exécuter 'source $RC_FILE' pour appliquer les changements."
+printf "\e[34m[PROGRAM]: \e[37mInstallation terminée. Veuillez redémarrer votre shell ou exécuter 'source $RC_FILE' pour appliquer les changements."
